c33819e0ac2c17059f6006e141c8764f08e6ac8f
Estecsky <apsurtec@gmail.com>
2025-04-08 12:13:29 +0800

    add SaveUseAutoSmooth

--- addonPreferences.py
+++ addonPreferences.py
@@ -47,6 +47,7 @@ class HD2_AddonPreferences(bpy.types.AddonPreferences):
     ShowArchivePatchPath : BoolProperty(name="ShowArchivePatchPath",default = False,description = "实时显示活动Archive和Patch的路径")
     ShowZipPatchButton : BoolProperty(name="ShowZipPatchButton",default = False,description = "显示打包Patch为Zip功能")
     Layout_search_New : BoolProperty(name="Layout_search_New",default = False,description = "显示搜索已知Archive为主的布局")
+    SaveUseAutoSmooth : BoolProperty(name="SaveUseAutoSmooth",default = True,description = "保存网格时使用自动平滑并将平滑角度设为180度，视觉上将与平滑着色一致，关闭则不对保存网格做任何调整")
     def draw(self, context):
 
         # layout = self.layout
@@ -56,6 +57,7 @@ class HD2_AddonPreferences(bpy.types.AddonPreferences):
         layout.prop(self, "ShowArchivePatchPath",text="实时显示活动Archive和Patch的路径")
         layout.prop(self, "ShowZipPatchButton",text="显示打包Patch为Zip功能")
         layout.prop(self, "Layout_search_New",text="显示搜索已知Archive为主的布局")
+        layout.prop(self, "SaveUseAutoSmooth",text="保存网格时开启自动平滑")
         addon_updater_ops.update_settings_ui(self, context)
 
 def register():
